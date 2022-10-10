5c7ad85074badf24b31959d2416daf4946eb88b2
federossi93 <rossifederico0993@gmail.com>
2022-10-10 17:10:09 +0200

    fix %15

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const li = document.createElement('li')
 for(let i = 1; i < 101 ; i++){
     console.log(i);
     
-    if(i % 15 == 0){
+    if(i % 3 == 0 && i % 5 ==0){
         console.log('FizzBuzz');
         li.append( 'FrizzBuzz ')
         ul.append(li)
