7784baf70f344b93c1ca70333b459547a460d4a7
federossi93 <rossifederico0993@gmail.com>
2022-10-10 14:01:37 +0200

    add loops

---
+++ main.js
@@ -0,0 +1,20 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero
+// e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+for(let i = 1; i < 101 ; i++){
+    console.log(i);
+    
+    if(i % 15 == 0){
+        console.log('FizzBuzz');
+    }
+    else if(i % 5 == 0){
+        console.log('buzz');
+    }
+    else if(i % 3 == 0){
+        console.log('Fizz');
+    }
+    
+
+}
