bba47918917136f5c3e4b2ad08c6debc4297500a
federossi93 <rossifederico0993@gmail.com>
2022-10-10 18:05:07 +0200

    add css

--- main.js
+++ main.js
@@ -5,34 +5,28 @@
 
 const ul = document.getElementById("list")
 
-const li = document.createElement('li')
-
-
 for(let i = 1; i < 101 ; i++){
     console.log(i);
+
+    const li = document.createElement('li')
+
+    li.append(i)
+    ul.append(li)
     
     if(i % 3 == 0 && i % 5 ==0){
         console.log('FizzBuzz');
-        li.append( 'FrizzBuzz ')
-        ul.append(li)
+        li.innerHTML = 'FizzBuzz'
+        li.style.backgroundColor ='red'
+        
     }
     else if(i % 5 == 0){
         console.log('buzz');
-        li.append( 'buzz ')
-        ul.append(li)
+        li.innerHTML = 'Buzz'
+        li.style.backgroundColor ='yellow'
     }
     else if(i % 3 == 0){
         console.log('Fizz');
-        li.append( 'fizz ')
-        ul.append(li)
+        li.innerHTML = 'Fizz'
+        li.style.backgroundColor ='green'
     }
-
-    else if(i % i == 0){
-        console.log('Fizz');
-        li.append( i + ' ')
-        ul.append(li)
-    }
-    
-    
-
 }
